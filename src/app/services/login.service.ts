d972864c4051d3222be93b8c2c3cae03e42665dc
Maxbwee <maxbowen1234@gmail.com>
2023-01-29 16:37:19 +0200

    Added login service for creating a user

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,19 +1,61 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable, of, switchMap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user.model';
+
+const { apiUsers, apiKey } = environment;
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  // Dependancy injection HTTP client into login service
+  // Tells what type of serviec it is injecting into the file
+  constructor(private readonly http: HttpClient) { }
 
   // Models, Observables, and RxJS operators
-
+  public login(username: string): Observable<User>{
+    return this.checkUsername(username)
+    .pipe(
+      switchMap((user: User | undefined) => {
+        if(user === undefined) {
+          // user does not exist
+          return this.createUser(username);
+        }
+        return of(user);
+      })
+    )
+  }
   // Login 
 
   // Check if user exists
+  private checkUsername(username: string): Observable<User | undefined> {
+    return this.http.get<User[]>(`${apiUsers}?username=${username}`)
+      .pipe(
+        //.Pipe function can use rxjs operators
+        map(( response: User[]) => response.pop())
+      )
+  }  
 
   // IF NOT user exists - Create a user
+  private createUser(username: string): Observable<User>{
+    // create user
+    const user = {
+      username, 
+      pokemon: []
+    };
+    // headers -> API key
+    const headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "x-api-key" : apiKey
+    });
+    // Post request to create an item on the server
 
+    return this.http.post<User>(apiUsers, user, {
+      headers
+    })
+  }
   // IF User || Created User exists - Store a user
 }
