c8fbb93217ac40b2f532e97b0ff8d9453079bcd5
Maxbwee <maxbowen1234@gmail.com>
2023-01-29 15:33:07 +0200

    Added same environments as Aino

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     apiUsers: "https://noroff-api-production-5783.up.railway.app/trainers",
     apiPokemon: "https://pokeapi.co/api/v2/pokemon/"
 };
