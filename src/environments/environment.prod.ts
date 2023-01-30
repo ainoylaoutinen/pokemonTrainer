10fb0c9b46e5f67fa0b14520d0e0539a65236d19
Aino Ylä-Outinen <aino.yla.outinen@fi.experis.com>
2023-01-30 14:18:40 +0200

    changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUsers: "https://noroff-api-production-5783.up.railway.app/trainers",
+    apiPokemons: "https://pokeapi.co/api/v2/pokemon/",
+    apiKey: "q9a9GEjhYk28rXWuYJRodw=="
+};
